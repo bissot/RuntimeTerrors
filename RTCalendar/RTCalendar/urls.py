73a55b7b7d541d51c594a084bfd8dbc8c811e682
Trevor Bisson <Tbisson@corebridge.net>
2020-12-07 18:43:14 -0500

    changed routing to making landing page work

--- RTCalendar/RTCalendar/urls.py
+++ RTCalendar/RTCalendar/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('calendarModule/', include('calendarModule.urls')),
+    path('', include('calendarModule.urls')),
     path('admin/', admin.site.urls),
 ]
